a70431c31e1362205452b2508a28ad6ab4d84f45
Lucas-Magalhaes-Sodre <lucas10maglhaessodre@gmail.com>
2024-05-29 00:33:36 -0300

    chore: add graphic

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,12 +1,41 @@
 import React from 'react';
 
-import { Box, Grid } from '@mui/material';
+import { Box, Chip, Grid } from '@mui/material';
 import DefaultPage from '@src/templates/DefaultPage';
 import { setores } from '@src/utils/constants';
+import Chart from 'react-apexcharts';
 
 import ButtonFilter from '@src/components/ButtonFilter';
 
 const HomePage = () => {
+  var options = {
+    series: [44, 100, 13, 43, 22],
+    chart: {
+      width: 400,
+      type: 'pie',
+    },
+    labels: [
+      'Licença Paternidade',
+      'Trabalhando',
+      'Licença Maternidade',
+      'Atestado',
+      'Férias',
+    ],
+    responsive: [
+      {
+        breakpoint: 480,
+        options: {
+          chart: {
+            width: 400,
+          },
+          legend: {
+            position: 'bottom',
+          },
+        },
+      },
+    ],
+  };
+
   return (
     <DefaultPage pageTitle="Início">
       <Grid container spacing={2}>
@@ -16,6 +45,19 @@ const HomePage = () => {
               <ButtonFilter key={item.value} textButton={item.name} />
             ))}
           </Box>
+        </Grid>
+
+        <Grid item xs={6}>
+          <Chip label="Small" size="small" />
+        </Grid>
+
+        <Grid item xs={6}>
+          <Chart
+            options={options}
+            series={options.series}
+            type="pie"
+            width={500}
+          />
         </Grid>
       </Grid>
     </DefaultPage>
