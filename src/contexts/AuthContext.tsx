70c6f58ad04250bf756000d857274fe10667e864
Ketlen Batista <115804201+ketlen-Batista@users.noreply.github.com>
2024-08-04 20:15:09 -0300

    feat: refresh token implemented

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,3 +1,120 @@
+// import React from 'react';
+// import { createContext, useEffect, useState } from 'react';
+// import { UserDTO } from '@dtos/UserDTO';
+// import { api } from '@src/lib/axios';
+// import {
+//   storageAuthTokenGet,
+//   storageAuthTokenRemove,
+//   storageAuthTokenSave,
+// } from '@storage/storageAuthToken';
+// import {
+//   storageUserGet,
+//   storageUserRemove,
+//   storageUserSave,
+// } from '@storage/storageUser';
+// import { getImageUrlServer } from '@utils/functions';
+// export type AuthContextDataProps = {
+//   user: UserDTO;
+//   signIn: (email: string, password: string) => Promise<void>;
+//   isLoadingStorageData: boolean;
+//   signOut: () => Promise<void>;
+//   userPhoto: string;
+// };
+// type AuthContextProviderProps = {
+//   children: React.ReactNode;
+// };
+// export const AuthContext = createContext<AuthContextDataProps>(
+//   {} as AuthContextDataProps,
+// );
+// export function AuthContextProvider({ children }: AuthContextProviderProps) {
+//   const [user, setUser] = useState<UserDTO>({} as UserDTO);
+//   const [isLoadingStorageData, setIsLoadingStorageData] = useState(true);
+//   const [userPhoto, setUserPhoto] = useState<string>('');
+//   console.log('entrou');
+//   async function headerUserAndTokenUpdate(userData: UserDTO, token: string) {
+//     api.defaults.headers.common['Authorization'] = 'Bearer ' + token;
+//     setUser(userData);
+//   }
+//   async function storageUserAndTokenSave(userData: UserDTO, token: string) {
+//     try {
+//       setIsLoadingStorageData(true);
+//       await storageUserSave(userData);
+//       await storageAuthTokenSave(token);
+//     } catch {
+//       console.log('error storageUserAndTokenSave');
+//     } finally {
+//       setIsLoadingStorageData(false);
+//     }
+//   }
+//   async function signIn(email: string, password: string) {
+//     try {
+//       setIsLoadingStorageData(true);
+//       const response = await api.post('/sessions', { email, password });
+//       const { token, refreshToken, user: userData } = response.data;
+//       if (userData && token) {
+//         await storageUserAndTokenSave(userData, token);
+//         headerUserAndTokenUpdate(userData, token);
+//       }
+//     } catch {
+//       // throw error;
+//       console.log('error');
+//     } finally {
+//       setIsLoadingStorageData(false);
+//     }
+//   }
+//   async function signOut() {
+//     try {
+//       setIsLoadingStorageData(true);
+//       setUser({} as UserDTO);
+//       await storageUserRemove();
+//       await storageAuthTokenRemove();
+//     } catch {
+//       // throw error;
+//       console.log('error');
+//     } finally {
+//       setIsLoadingStorageData(false);
+//     }
+//   }
+//   async function loadUserData() {
+//     try {
+//       setIsLoadingStorageData(true);
+//       const userLogged = await storageUserGet();
+//       const token = await storageAuthTokenGet();
+//       if (token && userLogged) {
+//         headerUserAndTokenUpdate(userLogged, token);
+//       }
+//     } catch {
+//       console.log('error loadUserData');
+//     } finally {
+//       setIsLoadingStorageData(false);
+//     }
+//   }
+//   function getPhotoUser(imageAvatarId: number) {
+//     setUserPhoto(getImageUrlServer(imageAvatarId));
+//   }
+//   useEffect(() => {
+//     if (user.photo_avatar_id) {
+//       getPhotoUser(user.photo_avatar_id);
+//     }
+//   }, [user.photo_avatar_id]);
+//   useEffect(() => {
+//     loadUserData();
+//   }, []);
+//   return (
+//     <AuthContext.Provider
+//       value={{
+//         isLoadingStorageData,
+//         user,
+//         signIn,
+//         signOut,
+//         userPhoto,
+//       }}
+//     >
+//       {children}
+//     </AuthContext.Provider>
+//   );
+// }
+////////////////////////////////////////////////
 import React from 'react';
 import { createContext, useEffect, useState } from 'react';
 
@@ -36,19 +153,23 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
   const [user, setUser] = useState<UserDTO>({} as UserDTO);
   const [isLoadingStorageData, setIsLoadingStorageData] = useState(true);
   const [userPhoto, setUserPhoto] = useState<string>('');
-  console.log('entrou');
+
   async function headerUserAndTokenUpdate(userData: UserDTO, token: string) {
     api.defaults.headers.common['Authorization'] = 'Bearer ' + token;
 
     setUser(userData);
   }
 
-  async function storageUserAndTokenSave(userData: UserDTO, token: string) {
+  async function storageUserAndTokenSave(
+    userData: UserDTO,
+    token: string,
+    refreshToken: string,
+  ) {
     try {
       setIsLoadingStorageData(true);
 
       await storageUserSave(userData);
-      await storageAuthTokenSave(token);
+      await storageAuthTokenSave({ token, refresh_token: refreshToken });
     } catch {
       console.log('error storageUserAndTokenSave');
     } finally {
@@ -63,8 +184,8 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 
       const { token, refreshToken, user: userData } = response.data;
 
-      if (userData && token) {
-        await storageUserAndTokenSave(userData, token);
+      if (userData && token && refreshToken) {
+        await storageUserAndTokenSave(userData, token, refreshToken);
         headerUserAndTokenUpdate(userData, token);
       }
     } catch {
@@ -94,7 +215,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     try {
       setIsLoadingStorageData(true);
       const userLogged = await storageUserGet();
-      const token = await storageAuthTokenGet();
+      const { token } = await storageAuthTokenGet();
 
       if (token && userLogged) {
         headerUserAndTokenUpdate(userLogged, token);
@@ -119,6 +240,14 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
   useEffect(() => {
     loadUserData();
   }, []);
+
+  useEffect(() => {
+    const subscribe = api.registerInterceptTokenManager(signOut);
+
+    return () => {
+      subscribe();
+    };
+  }, [signOut]);
 
   return (
     <AuthContext.Provider
--- src/storage/storageAuthToken.ts
+++ src/storage/storageAuthToken.ts
@@ -1,15 +1,38 @@
-import { AUTH_TOKEN_STORAGE } from "./storageConfig";
+// import { AUTH_TOKEN_STORAGE } from "./storageConfig";
 
-export async function storageAuthTokenSave(token) {
-  localStorage.setItem(AUTH_TOKEN_STORAGE, token);
+// export async function storageAuthTokenSave(token) {
+//   localStorage.setItem(AUTH_TOKEN_STORAGE, token);
+// }
+
+// export async function storageAuthTokenGet() {
+//   const token = localStorage.getItem(AUTH_TOKEN_STORAGE);
+
+//   return token;
+// }
+
+// export async function storageAuthTokenRemove() {
+//   localStorage.removeItem(AUTH_TOKEN_STORAGE);
+// }
+
+
+import { USER_STORAGE } from "./storageConfig";
+
+type StorageAuthTokenProps = {
+  token: string;
+  refresh_token: string;
+}
+export async function storageAuthTokenSave({token, refresh_token}: StorageAuthTokenProps) {
+  await localStorage.setItem(USER_STORAGE, JSON.stringify({token, refresh_token}) );
 }
 
 export async function storageAuthTokenGet() {
-  const token = localStorage.getItem(AUTH_TOKEN_STORAGE);
+  const response = await localStorage.getItem(USER_STORAGE);
 
-  return token;
+  const {token, refresh_token}: StorageAuthTokenProps = response ? JSON.parse(response) : {}
+
+  return {token, refresh_token};
 }
 
 export async function storageAuthTokenRemove() {
-  localStorage.removeItem(AUTH_TOKEN_STORAGE);
+  await localStorage.removeItem(USER_STORAGE);
 }
